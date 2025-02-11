b45ff24f35b610ad50eef07e2263fd52469c7e8f
CLMFAP <dgmun94@gmail.com>
2025-02-11 14:23:33 -0800

    update CLMFAP

--- CLMFAP_Ver.2/ablation/run_task_retrosys.sh
+++
@@ -1,6 +0,0 @@
-
-python task_retrosys_generation.py \
-        --smiles_graph_weight 0.2 \
-        --smiles_fp_weight 0.4 \
-        --graph_fp_weight 0.4 \
-        --graph_model 'bi_graphormer_mpnn'
